d943edbc5ab6b70abaaafa6692e4325d796ca7e0
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2021-08-31 21:35:19 +0200

    :wrench: Add support for ES Modules in node-fetch

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,9 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest'
   },
+  transformIgnorePatterns: ['node_modules/(?!(node-fetch|fetch-blob)/)'],
   verbose: true
 }
